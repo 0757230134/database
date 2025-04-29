2274c943098b08c10fbaaa971f1445730501df4b
Justus <jades@xyz.com>
2025-04-29 10:50:19 +0300

    savin_dayta

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,10 @@
+from itertools import count
+from tkinter.font import names
+
 from django.shortcuts import render, redirect
+
+from my_app.models import Person
+
 
 # Create your views here.
 
@@ -11,6 +17,12 @@ def submit(request):
         email = request.POST['email']
         dob = request.POST['dob']
         phone = request.POST['phone']
-        print(name, email, dob, phone)
+        weight = request.POST['weight']
+        height = request.POST['height']
+        gender = request.POST['gender']
+        print(name, email, dob, phone, weight ,height, gender)
+        Person.objects.create(name=names, email=email, dob=dob, phone=phone,weight=weight, height=height, gender=gender)
+        count = Person.objects.all().count()
+        print(f"You have {count} records")
     return redirect('home-page')
 
